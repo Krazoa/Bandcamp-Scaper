e330e965dfe83608c8c30c9638946a904fe0421c
Michele <krazoafox89@gmail.com>
2018-03-31 20:59:11 +1030

    Script tag (containing the mp3-128 file) filtering added.
    Now to single the one of 3 mp3-128 URLs out. Fun.

--- BandCampSpider.py
+++ BandCampSpider.py
@@ -32,8 +32,6 @@ while validationCondition == 0:
 #Scrape album page**
 response = requests.get(albumURL) #non-object var
 soup = BeautifulSoup(response.content, "lxml")
-##with open(albumURL) as html_file: #object var
-##    soup = str(BeautifulSoup(html_file, "lxml"))
 #print(str(soup)) #DEBUG: Checking to see if HTML was successfully grabbed
 
 #download album cover** (WORKS)
@@ -42,32 +40,51 @@ soup = BeautifulSoup(response.content, "lxml")
 ##urllib.request.urlretrieve(coverUrl, downloadDestination + "\cover.jpg")
 ##print("Album cover downloaded successfully")
 
+##getting raw bandcamp name
+slashCount = 0
+artistURL = ""
+index = 0
+while slashCount < 3:
+    if albumURL[index] == "/":
+        slashCount += 1
+    if slashCount != 3:
+        artistURL += albumURL[index]
+    index += 1
+#print(artistURL) #DEBUG: Checking the reconstructed bandcamp URL
+
 #downloading tracks**
 noOfTracks = 0
 trackList = list()
 trackNameList = list()
 tracks = soup.find_all("a", href=re.compile("/track/"),itemprop="url")
 #print(tracks) #DEBUG: Checking what tags were scrapped
-##albumDesc = soup.find("meta", attrs={"name":"Description"})
-##print(albumDesc)
+##albumDesc = soup.find("meta", attrs={"name":"Description"}) #Scraping the discription: unused now
+##print(albumDesc)                                            #kept here for legacy reasons
 ##tracksOnAlbum = albumDesc.get("content")
-tracksOnAlbum = soup.find_all("span", itemprop={"name"}, text=True) #Solution 2
+tracksOnAlbum = soup.find_all("span", itemprop={"name"}, text=True)
 #print(tracksOnAlbum)
 for trackName in tracksOnAlbum:
     trackNameList.append(trackName.find_all(text=True))
 trackLimit = len(tracksOnAlbum)
-print(trackLimit)
-for i in range(0, trackLimit): 
-    trackList.append(tracks[i].get("href"))
-    print(trackNameList[i]) #DEBUG: Checking if track names were extracted...
-    print(trackList[i]) #DEBUG: ...along with their respective track URLs
+#print(trackLimit) #DEBUG: Shows number of tracks on album
+for i in range(0, 1): #####replace with trackLimit
+    trackList.append(tracks[i].get("href"))#REMEMBER: The soup is an array/list
+    #print(trackNameList[i]) #DEBUG: Checking if track names were extracted...
+    #print(trackList[i]) #DEBUG: ...along with their respective track URLs
+    #print(artistURL + trackList[i]) #DEBUG: Checking if track URL was successfully combined
+    responseTrack = requests.get(artistURL + trackList[i])
+    soup = BeautifulSoup(responseTrack.content, "lxml")
+    trackJavaScript = soup.find_all("script", attrs={"type":"text/javascript"}, text=re.compile("t4.bcbits.com")) #Original method
+    #print(trackJavaScript[7]) #Index value is hard coded as the 7th "script" tag contains the mp3-128 url
+    #trackMp3128URL = trackJavaScript[7].get_text("mp3-128")
     
+    #if the programs runs like stale shit sliding down a hill, its because of this
+    for letter 
     
+    #print(trackMp3128URL)
+
+
     
 #change albumUrl into albumTrackUrl (See notes)
-#for i in range(0, 10): #Change max range to number of songs in album (Duplicate, consider merging this for loop in the above for loop
-    #Open each link
-        #fetch the track source
-        #download it
 print("Done")
 
