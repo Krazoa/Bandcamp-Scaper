22a540efb4da644a62cf8c091b09a01eb43ebb19
Krazoa <Krazoa@users.noreply.github.com>
2018-03-31 17:08:33 +1030

    Track name & URL scraper added
    Now scrapes all track names in the album and their respective page URLs

--- BandCampSpider.py
+++ BandCampSpider.py
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup
 import requests
 from lxml import etree
 import urllib.request
+import re
+import time
 
 #get target album URL**
 albumURL = input("Enter target album URL: ")
@@ -27,7 +29,6 @@ while validationCondition == 0:
         validationCondition = 0
 #else:
     #print("DEBUG: Loop Broken")
-
 #Scrape album page**
 response = requests.get(albumURL) #non-object var
 soup = BeautifulSoup(response.content, "lxml")
@@ -35,26 +36,38 @@ soup = BeautifulSoup(response.content, "lxml")
 ##    soup = str(BeautifulSoup(html_file, "lxml"))
 #print(str(soup)) #DEBUG: Checking to see if HTML was successfully grabbed
 
-#Create a folder for the files to be downloaded in**
-#fetch album title
-##albumTitle = soup.head2
-##print(albumTitle)
-
-#download album cover**
-coverTag = soup.find("img", itemprop="image") #get album cover URL
-coverUrl = coverTag.get("src") #assign it to coverURL
-##coverResponse = requests.get(coverUrl, stream=True)#download the cover to destination folder
-##with open(downloadDestination, "w", opener=opener) as file:#deposit it into the download destination as 'cover.jpg'
-##    shutil.copyfileobj(coverResponse.raw, downloadDestination)
-print(downloadDestination + "\cover.jpg")
-urllib.request.urlretrieve(coverUrl, downloadDestination + "\cover.jpg")
+#download album cover** (WORKS)
+##coverTag = soup.find("img", itemprop="image") #get album cover URL
+##coverUrl = coverTag.get("src") #assign it to coverURL
+##urllib.request.urlretrieve(coverUrl, downloadDestination + "\cover.jpg")
+##print("Album cover downloaded successfully")
 
 #downloading tracks**
-#get number of tracks in album
-#assign each track an index number
-#for each index number in the album track list...
-    #...open each URL Source for each track...
-    #play music file (to enable streaming to the player, enabling download)
-    #download the file to the download destination file address
-#print 'done'
+noOfTracks = 0
+trackList = list()
+trackNameList = list()
+tracks = soup.find_all("a", href=re.compile("/track/"),itemprop="url")
+#print(tracks) #DEBUG: Checking what tags were scrapped
+##albumDesc = soup.find("meta", attrs={"name":"Description"})
+##print(albumDesc)
+##tracksOnAlbum = albumDesc.get("content")
+tracksOnAlbum = soup.find_all("span", itemprop={"name"}, text=True) #Solution 2
+#print(tracksOnAlbum)
+for trackName in tracksOnAlbum:
+    trackNameList.append(trackName.find_all(text=True))
+trackLimit = len(tracksOnAlbum)
+print(trackLimit)
+for i in range(0, trackLimit): 
+    trackList.append(tracks[i].get("href"))
+    print(trackNameList[i]) #DEBUG: Checking if track names were extracted...
+    print(trackList[i]) #DEBUG: ...along with their respective track URLs
+    
+    
+    
+#change albumUrl into albumTrackUrl (See notes)
+#for i in range(0, 10): #Change max range to number of songs in album (Duplicate, consider merging this for loop in the above for loop
+    #Open each link
+        #fetch the track source
+        #download it
+print("Done")
 
