fd4fe4a8733758f4ab68b66c9d74958b30a289d1
Malfestio <krazoafox89@gmail.com>
2018-08-01 08:04:03 +0930

    Continuation of rebuild
    About 65% complete.

--- downloader.py
+++ downloader.py
@@ -4,6 +4,7 @@ import os
 from bs4 import BeautifulSoup
 from lxml import etree
 import requests
+import urllib.request
 import re
 
 def Soup(url):
@@ -25,7 +26,12 @@ def userInpt():
         downloadURL = input("Enter download destination: ")
         userValidation = input("Confirm download destination [y/n]: ")
         if userValidation == "y":
-            validationCondition = False
+            directoryValidation = os.path.exists(downloadURL)
+            if directoryValidation == False:
+                print("Invalid directory. Check your input directory and try again")
+            else:
+                print("Directory successfully validated")
+                validationCondition = False
             
     return [albumURL, downloadURL]        
 
@@ -35,13 +41,13 @@ def retrieve(soup, downloadURL, givenTag1, givenTag2, extension):
     #Find file from item source
     itemURL = itemTag.get("src")
     #Retrieve item source
-    if itemURL != ""
+    if itemURL != "":
         urllib.request.urlretrieve(itemURL, str(downloadURL + extension))
         return True
     else:
         return False
 
-def getName():
+def getName(albumURL):
     slashCount = 0
     artistURL = ""
     index = 0
@@ -54,13 +60,17 @@ def getName():
     return artistURL
 
 def illegalCharCheck(inptString):
-    illegalChars = ["\\", "/", ":", "*", "?", '"', "<", ">", "|"]
+    illegalChars = ["\\", "/", ":", "*", "?", '"', "<", ">", "|"] #Potential bug with checking the "\\" char
     newString = ""
+    invalidChar = False
     for index in range(0, len(inptString)):
         for illegalChar in illegalChars:
-            if char == illegalChar:
-                newChar = "
-        newString
+            if inptString[index] == illegalChar:
+                newString += "-"
+                invalidChar = True
+        if invalidChar == True:
+            newString += inptString[index]
+            invalidChar == False
     return newString
 
 #Get input and create soup
@@ -76,5 +86,31 @@ else:
     print("Error: Item not found. Check tags.")
 
 #Get album data
-artistURL = getName()  
+artistURL = getName(userInput[0])
+print(artistURL)
 
+#Building track list
+noOfTracks = 0
+trackList = list()
+trackNameList = list()
+trackNameListStr = list()
+tracks = soup.find_all("a", href=re.compile("/track/"),itemprop="url")
+
+tracksOnAlbum = soup.find_all("span", itemprop={"name"}, text=True)
+for trackName in tracksOnAlbum:
+    trackNameList.append(trackName.find_all(text=True, recursive=False))
+trackLimit = len(tracksOnAlbum)
+
+for iStr in range(0, len(tracksOnAlbum)):
+    trackNameStrStore = str(trackNameList[iStr])
+    trackCharIndex = 0
+    trackNameStr = ""
+    while trackCharIndex != len(trackNameStrStore):
+        if trackNameStrStore[trackCharIndex] == "[" or trackNameStrStore[trackCharIndex] == "]":
+            pass
+        elif trackNameStrStore[trackCharIndex] == "'" or trackNameStrStore[trackCharIndex] == "'":
+            pass
+        else:
+            trackNameStr += trackNameStrStore[trackCharIndex]
+        trackCharIndex += 1
+    trackNameListStr.append(trackNameStr)
