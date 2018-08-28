c61ab7b7ef7ad4d764b95fee6787973a5bb3615a
triggerhappyhavoc <triggerhappyhavoc@users.noreply.github.com>
2018-08-28 10:24:08 +0930

    Revert to version 1 of the script.
    Borked code has been here for a while. Time for borked code to begone.
    
    I'll get around to fixing this code again when I have the time.
    In the meantime, have the original quick fix spider I built. It mostly works at least.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Bandcamp-Spider
 
 A spider which downloads the preview versions of songs from bandcamp in MP3 format with a (low) bitrate of 128kb/s.
+
+The current version of this is very dirty hack. It does have a tendency to crash with some song names (ones with reserved or special characters in the url) and is generally unstable with some crashes that I have yet to determine the cause of. I might fix this and clean the code up in the future, but for now it works (mostly).
+
 Disclaimer: Use this at own risk. I'm not responsible for the damages caused by this program if you use it.
 
 If you're worried about the legality of it: https://bandcamp.com/help/audio_basics#steal
-
--- downloader.py
+++ downloader.py
@@ -1,138 +1,184 @@
-#Code by: Malfestio
+#Code by: Krazoa
+#Bandcamp Spider: v0.1
 
+import shutil
 import os
+import sys
 from bs4 import BeautifulSoup
-from lxml import etree
 import requests
+from lxml import etree
 import urllib.request
 import re
+import time
 
-def Soup(url):
-    response = requests.get(url)
-    return BeautifulSoup(response.content, "lxml")
-
-def userInpt():
-    #Album URL input
-    validationCondition = True
-    while validationCondition == True:
-        albumURL = input("Enter album URL: ")
-        userValidation = input("Confirm album URL [y/n]: ")
-        if userValidation == "y":
-            validationCondition = False
-
-    #Download destination input
-    validationCondition = True
-    while validationCondition == True:
-        downloadURL = input("Enter download destination: ")
-        userValidation = input("Confirm download destination [y/n]: ")
-        if userValidation == "y":
-            directoryValidation = os.path.exists(downloadURL)
-            if directoryValidation == False:
-                print("Invalid directory. Check your input directory and try again")
-            else:
-                print("Directory successfully validated")
-                validationCondition = False
-            
-    return [albumURL, downloadURL]
-
-def download(itemURL, downloadURL, extension):
-    if itemURL != "":
-        print(str(downloadURL + extension))
-        urllib.request.urlretrieve(itemURL, str(downloadURL + extension))
-        return True
+#get target album URL**
+albumURL = input("Enter target album URL: ")
+validationCondition = 0
+#get download destination file address (where track will be downloaded to
+while validationCondition == 0:
+    downloadDestination = input("Enter URL of download destination on your computer: ")
+    #check download destination and verify its existance
+    downloadConfirm = input("Are you sure this is your download directory? This cannot be changed. Please type [confirm] or press enter to cancel: ").upper()
+    if downloadConfirm == "CONFIRM":
+        directoryValidation = os.path.exists(downloadDestination)
+        if directoryValidation == False:
+            print("Invalid directory. Check your input directory and try again")
+        else:
+            print("Directory successfully validated")
+            validationCondition = 1
     else:
-        return False
-
-def parseItemSource(soup, downloadURL, givenTag1, givenTag2, extension):
-    #Find item using given tags
-    itemTag = soup.find(givenTag1, itemprop=givenTag2)
-    #Find file from item source
-    itemURL = itemTag.get("src")
-    #Retrieve item source
-    return download(itemURL, downloadURL, extension)
-
-def getName(albumURL):
-    slashCount = 0
-    artistURL = ""
-    index = 0
-    while slashCount < 3:
-        if albumURL[index] == "/":
-            slashCount += 1
-        if slashCount != 3:
-            artistURL += albumURL[index]
-        index += 1
-    return artistURL
-
-def illegalCharCheck(inptString):
-    illegalChars = ["\\", "/", ":", "*", "?", '"', "<", ">", "|", " "] #Potential bug with checking the "\\" char
-    newString = ""
-    invalidChar = False
-    for index in range(0, len(inptString)):
-        invalidChar = False
-        for illegalChar in illegalChars:
-            if inptString[index] == illegalChar:
-                newString += "-"
-                invalidChar = True
-        if invalidChar == False:
-            newString += inptString[index]
-    return newString
-
-def buildTrackList(soup):
-    noOfTracks = 0
-    trackList = list()
-    trackNameList = list()
-    trackNameListStr = list()
-    tracks = soup.find_all("a", href=re.compile("/track/"),itemprop="url")
-
-    tracksOnAlbum = soup.find_all("span", itemprop={"name"}, text=True)
-    for trackName in tracksOnAlbum:
-        trackNameList.append(trackName.find_all(text=True, recursive=False))
-
-    for iStr in range(0, len(tracksOnAlbum)):
-        trackNameStrStore = str(trackNameList[iStr])
-        trackCharIndex = 0
-        trackNameStr = ""
-        while trackCharIndex != len(trackNameStrStore):
-            if trackNameStrStore[trackCharIndex] == "[" or trackNameStrStore[trackCharIndex] == "]":
-                pass
-            elif trackNameStrStore[trackCharIndex] == "'" or trackNameStrStore[trackCharIndex] == "'":
-                pass
-            else:
-                trackNameStr += trackNameStrStore[trackCharIndex]
-            trackCharIndex += 1
-        trackNameListStr.append(illegalCharCheck(trackNameStr))
+        validationCondition = 0
         
-    return trackNameListStr
-
-#Get input and create soup
-userInput = userInpt()
-soup = Soup(str(userInput[0]))
-
-#Get album cover
-print("Getting album cover")
-operationState = parseItemSource(soup, str(userInput[1]), "img", "image", "\cover.jpg")
-if operationState == True:
-    print("Album cover downloaded successfully!")
-else:
-    print("Error: Item not found. Check tags.")
-
-#Get album data
-artistURL = getName(userInput[0])
-##print(artistURL)
-
-#Building track list
-trackList = buildTrackList(soup)
-##print(trackList)
-
-#Download all tracks from constructed track list
-for i in range(0, len(trackList)):
-    print("Downloading track", i, " - ", trackList[i])
-    print((artistURL + "/track/" +trackList[i]), "\n" + str(userInput[1]) + ("\\" + trackList[i] + ".mp3"))
-    operationState = download((artistURL + "/track/" +trackList[i]), str(userInput[1]), ("\\" + trackList[i] + ".mp3"))
-    if operationState == True:
-        print("Track ", i, " - ", trackList[i])
-    else:
-        print("Error: Item not found. Check tags.")
+#print("DEBUG: Loop Broken")
+####Timer module
+taskTimeStart = 0
+taskTimeEnd = 0
+taskTimeDiff = 0
+taskTimeTotal = 0
     
-print("Download complete!\nFiles have been saved to", downloadURL)
-    
+        
+#Scrape album page**
+print("Getting album page HTML...")
+taskTimeStart = time.time()
+response = requests.get(albumURL) #non-object var
+soup = BeautifulSoup(response.content, "lxml")
+#print(str(soup)) #DEBUG: Checking to see if HTML was successfully grabbed
+#taskTimeList.append(taskTimeStart - taskTimeEnd)
+print("Got album page HTML")
+taskTimeEnd = time.time()
+taskTimeDiff = float(taskTimeEnd - taskTimeStart)
+print("Operation completed in ", taskTimeDiff, "s")
+taskTimeTotal += taskTimeDiff
+
+#download album cover
+print("Downloading album cover...")
+taskTimeStart = time.time()
+coverTag = soup.find("img", itemprop="image") #get album cover URL
+coverUrl = coverTag.get("src") #assign it to coverURL
+urllib.request.urlretrieve(coverUrl, downloadDestination + "\cover.jpg")
+print("Album cover downloaded successfully")
+taskTimeEnd = time.time()
+taskTimeDiff = float(taskTimeEnd - taskTimeStart)
+print("Operation completed in ", taskTimeDiff, "s")
+taskTimeTotal += taskTimeDiff
+
+##getting raw bandcamp name
+slashCount = 0
+artistURL = ""
+index = 0
+while slashCount < 3:
+    if albumURL[index] == "/":
+        slashCount += 1
+    if slashCount != 3:
+        artistURL += albumURL[index]
+    index += 1
+#print(artistURL) #DEBUG: Checking the reconstructed bandcamp URL
+
+#downloading tracks**
+noOfTracks = 0
+trackList = list()
+trackNameList = list()
+trackNameListStr = list()
+tracks = soup.find_all("a", href=re.compile("/track/"),itemprop="url")
+#print(tracks) #DEBUG: Checking what tags were scrapped
+##albumDesc = soup.find("meta", attrs={"name":"Description"}) #Scraping the discription to find no. of tracks: redundant now
+##print(albumDesc)                                            #kept here for legacy reasons
+##tracksOnAlbum = albumDesc.get("content")
+tracksOnAlbum = soup.find_all("span", itemprop={"name"}, text=True)
+#print(tracksOnAlbum)
+for trackName in tracksOnAlbum:
+    trackNameList.append(trackName.find_all(text=True, recursive=False))
+#print(trackNameList) #DEBUG: Show the list of track names
+trackLimit = len(tracksOnAlbum)
+
+for iStr in range(0, len(tracksOnAlbum)):
+    trackNameStrStore = str(trackNameList[iStr])
+    trackCharIndex = 0
+    trackNameStr = ""
+    while trackCharIndex != len(trackNameStrStore):
+        if trackNameStrStore[trackCharIndex] == "[" or trackNameStrStore[trackCharIndex] == "]":
+            pass
+        elif trackNameStrStore[trackCharIndex] == "'" or trackNameStrStore[trackCharIndex] == "'":
+            pass
+        else:
+            trackNameStr += trackNameStrStore[trackCharIndex]
+        trackCharIndex += 1
+    trackNameListStr.append(trackNameStr)
+#print(trackNameListStr) #DEBUG: Checking if the "[" and "'" elements are removed
+#print(trackLimit) #DEBUG: Shows number of tracks on album
+for i in range(0, trackLimit): #####replace with trackLimit for normal operation or 1 for testing
+    print("Downloading track ", i, " - ", trackNameListStr[i])
+    taskTimeStart = time.time()
+    trackList.append(tracks[i].get("href"))#REMEMBER: The soup is an array/list
+    #print(trackNameList[i]) #DEBUG: Checking if track names were extracted...
+    #print(trackList[i]) #DEBUG: ...along with their respective track URLs
+    #print(artistURL + trackList[i]) #DEBUG: Checking if track URL was successfully combined
+    responseTrack = requests.get(artistURL + trackList[i])
+    soup = BeautifulSoup(responseTrack.content, "lxml")
+    trackJavaScript = soup.find_all("script", attrs={"type":"text/javascript"}, text=re.compile("t4.bcbits.com")) #Original method
+    #print(trackJavaScript[0]) #Index value is hard coded as the 0th "script" tag contains one of the three mp3-128 urls
+    ############
+####REBUILD IS UP TO HERE#####
+    ############
+    if trackJavaScript == "":
+        print("BUG: trackMp3128Tag = trackJavaScript[0].get_text(\"mp3-128\")\nIndexError: list index out of range")
+        print("Skipping track")
+        break
+    trackMp3128Tag = trackJavaScript[0].get_text("mp3-128")
+    #print(len(trackMp3128Tag))
+
+    trackMp3128URL = ""
+    tagPhrase = ""
+    letter = 0
+    firstOddQuoteMark = True
+    #if the programs runs like stale shit sliding down a hill, it's because of this
+    #for letter in range (0, len(trackMp3128Tag) - 1): #Removed due to the resetting of 'letter' each time this was run
+    mp3128URLFound = False
+    while letter != len(trackMp3128Tag) - 1 and mp3128URLFound == False:
+        letter += 1
+        quoteMarkCount = 0
+        if trackMp3128Tag[letter] == '"': #If a new phrase is found, trigger phrase constructor
+##            if firstOddQuoteMark == True:
+##                quoteMarkCount = 2
+##                firstOddQuoteMark = False
+##            elif firstOddQuoteMark == False:
+                quoteMarkCount += 1
+                tagPhrase = ""
+                while quoteMarkCount < 2:
+                    letter += 1
+                    #for letterPhrase in range (letter, len(trackMp3128Tag) - 1):
+                    if trackMp3128Tag[letter] == '"':
+                        quoteMarkCount += 1
+                    else:
+                        tagPhrase += trackMp3128Tag[letter]
+        #print(tagPhrase) #DEBUG: Checking what the phrase constructor has built
+        #This if statement produces 2 valid variations of the mp3-128 url. It has been
+        #now set to only produce 1 but should be required, the other can be utilised
+        if tagPhrase == "mp3-128" and mp3128URLFound == False:
+            #print("Mp3-128 URL found")
+            Mp3128URLIndex = letter + 3
+            endQuoteMark = False
+            while endQuoteMark == False:
+                if trackMp3128Tag[Mp3128URLIndex] == '"':
+                    endQuoteMark = True
+                    letter = len(trackMp3128Tag) - 1
+                else:
+                    trackMp3128URL += trackMp3128Tag[Mp3128URLIndex]
+                    Mp3128URLIndex += 1
+            mp3128URLFound = True
+            #print(trackMp3128URL) #DEBUG: Checking if the url has been correctly found and placed in variable
+        if mp3128URLFound == True:
+            #downloading the mmp3-128 file
+            urllib.request.urlretrieve(trackMp3128URL, downloadDestination + "\\" + str(trackNameListStr[i]) + ".mp3")
+            print("Saved as: ", downloadDestination + "\\" + str(trackNameListStr[i]) + ".mp3")
+            letter = Mp3128URLIndex
+            taskTimeEnd = time.time()
+            taskTimeDiff = float(taskTimeEnd - taskTimeStart)
+            print("Operation completed in ", taskTimeDiff, "s")
+            taskTimeTotal += taskTimeDiff
+            
+print("Download Complete!")
+print("All files saved in ", downloadDestination)
+print("Total operation time: ", taskTimeTotal, "s")
+print("Done")
+
